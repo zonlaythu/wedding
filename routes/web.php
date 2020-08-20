581b321f8614f52748de0134e412498c04ab565d
zonlaythu <zonlay116873@gmail.com>
2020-08-20 22:02:38 +0630

    auth and backend dashboard

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// Backend
+// --------
+Route::middleware('role:admin')->group(function(){
+
+Route::get('/dashboard', 'BackendController@dashboard')->name('dashboard');
+
+});
+
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
